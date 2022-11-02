9f26ff520df1b731ec0a71ea6a3bdfbd3073f633
Shagaeva Elena <shagaevaelena@Air-Shagaeva.sknt.ru>
2022-11-02 14:25:24 +0300

    Removed storyboard

---
+++ СryptoWallet/AuthViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewController.swift
+//  СryptoWallet
+//
+//  Created by Shagaeva Elena on 02.11.2022.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+    }
+}
